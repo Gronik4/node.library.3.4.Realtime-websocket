433e81e76022391b427ed1bf49b2dfb3bb0b68f4
Gronik4 <golegni@yandex.ru>
2025-08-05 21:19:38 +0300

    Add socket

--- Utils/handler.js
+++
@@ -1,6 +0,0 @@
-const sendAll = document.getElementById('send-all');
-sendAll.addEventListener('click', clickSendAll);
-  function clickSendAll() {
-  alert('click on stnd all');
-}
-module.exports = clickSendAll;
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const notPage = require('./Middleware/notPage');
 
 const router = require('./Routes/index');
 const bookRouters = require('./Routes/books');
+const http = require('http');
+const socketIO = require('socket.io');
+
 
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 
@@ -14,8 +21,36 @@ app.use('/book', bookRouters);
 
 app.use(notPage);
 
+io.on('connection', (socket)=> {
+  const { id } = socket;
+  console.log(`Sockey connectsd ${id}`);
+
+  //сообщение себе
+  socket.on('message-to-me', (msg)=> {
+    msg.type = 'me';
+    socket.emit('message-to-me', msg);
+  })
+  //сообщение для всех 
+  socket.on('message-to-all', (msg)=> {
+    msg.type = 'all';
+    socket.broadcast.emit('message-to-all', msg);
+    socket.emit('message-to-all', msg);
+  })
+
+  socket.on('disconnect', ()=> {
+    console.log(`Socket disconnect ${id}`);
+  })
+})
+
 const PORT = process.env.PORT || 3006;
-app.listen(PORT, (err)=> {
+/*app.listen(PORT, (err)=> {
+  if(err) {
+    return console.log(`Server not starting - err: ${err}`);
+  } else {
+    console.log(`Server starting on port: ${PORT}`);
+  }
+})*/
+server.listen(PORT, (err)=> {
   if(err) {
     return console.log(`Server not starting - err: ${err}`);
   } else {
